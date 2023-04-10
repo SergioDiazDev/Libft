3d94b51a3783a0fd29bb479a20d306adc7beec74
sdiaz-ru <sdiaz-ru@student.42malaga.com>
2023-04-10 13:21:02 +0200

    Sigue dando un error en el estricto

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: sdiaz-ru <sdiaz-ru@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 12:23:49 by sdiaz-ru          #+#    #+#             */
-/*   Updated: 2022/10/18 13:38:10 by sdiaz-ru         ###   ########.fr       */
+/*   Updated: 2023/04/10 13:20:13 by sdiaz-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,15 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	if (!lst)
 		return (NULL);
 	result = ft_lstnew(f(lst->content));
+	if (!result)
+		return (NULL);
 	aux = result;
 	lst = lst->next;
 	while (lst)
 	{
 		result->next = ft_lstnew(f(lst->content));
-		if (result == NULL)
-			ft_lstclear(&result, del);
+		if (!result->next)
+			return (ft_lstclear(&aux, del), NULL);
 		result = result->next;
 		lst = lst->next;
 	}
