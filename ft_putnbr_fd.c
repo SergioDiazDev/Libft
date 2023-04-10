22ceb755ebde5440e273e7e3462a870b29721211
sdiaz-ru <sdiaz-ru@student.42malaga.com>
2023-04-10 12:31:19 +0200

    Eliminada reserva de memoria

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: sdiaz-ru <sdiaz-ru@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 16:55:29 by sdiaz-ru          #+#    #+#             */
-/*   Updated: 2023/03/08 19:42:03 by sdiaz-ru         ###   ########.fr       */
+/*   Updated: 2023/04/10 12:30:21 by sdiaz-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,19 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*aux;
+	long	num;
 
-	aux = ft_itoa(n);
-	ft_putstr_fd(aux, fd);
-	free(aux);
+	num = n;
+	if (num < 0)
+	{
+		ft_putchar_fd('-', fd);
+		num *= -1;
+	}
+	if ((num / 10) < 1)
+		ft_putchar_fd(num + '0', fd);
+	else
+	{
+		ft_putnbr_fd(num / 10, fd);
+		ft_putchar_fd((num % 10) + '0', fd);
+	}
 }
