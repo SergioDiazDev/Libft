970251bd9a2c8df64ebc1a4705f307ba66ca078e
sdiaz-ru <sdiaz-ru@student.42malaga.com>
2023-04-10 11:39:21 +0200

    memmove estricto

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: sdiaz-ru <sdiaz-ru@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 09:11:36 by sdiaz-ru          #+#    #+#             */
-/*   Updated: 2023/03/08 13:34:31 by sdiaz-ru         ###   ########.fr       */
+/*   Updated: 2023/04/10 11:38:06 by sdiaz-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,24 @@ void	*ft_memmove(void *dest, const void *src, t_size_t n)
 {
 	const char	*s;
 	char		*d;
+	t_size_t	i;
 	char		buf[65536];
 
 	s = src;
 	d = dest;
-	ft_memcpy(buf, s, n);
-	ft_memcpy(d, buf, n);
+	i = 0;
+	if ((!d && !s) || n > 65535)
+		return (NULL);
+	while (i < n)
+	{
+		buf[i] = s[i];
+		i++;
+	}
+	i = 0;
+	while (i < n)
+	{
+		d[i] = buf[i];
+		i++;
+	}
 	return (dest);
 }
